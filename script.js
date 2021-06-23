90f59850c7d55a767f2ef1439ffb00a7fcc50bb2
Andrew Smith <meuzishun@gmail.com>
2021-06-23 14:39:59 -0400

    Create functions for random choice and comparing choices

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Hello World!");
+function computerPlay() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let randIndex = Math.floor(Math.random() * choices.length);
+    return choices[randIndex];
+}
+
+function compare(userChoice, compChoice) {
+    if (userChoice === 'Rock') {
+        if (compChoice === 'Rock') {
+            return 'draw';
+        }
+        if (compChoice === 'Paper') {
+            return 'win';
+        }
+        if (compChoice === 'Scissors') {
+            return 'lose';
+        }
+    }
+    if (userChoice === 'Paper') {
+        if (compChoice === 'Rock') {
+            return 'win';
+        }
+        if (compChoice === 'Paper') {
+            return 'draw';
+        }
+        if (compChoice === 'Scissors') {
+            return 'lose';
+        }
+    }
+    if (userChoice === 'Scissors') {
+        if (compChoice === 'Rock') {
+            return 'lose';
+        }
+        if (compChoice === 'Paper') {
+            return 'win';
+        }
+        if (compChoice === 'Scissors') {
+            return 'draw';
+        }
+    }
+}
