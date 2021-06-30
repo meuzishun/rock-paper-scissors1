eb1dabc09c540b3b3f34c968d68460e2a7db9fc1
Andrew Smith <meuzishun@gmail.com>
2021-06-30 17:20:36 -0400

    Clean up comments

--- script.js
+++ script.js
@@ -97,35 +97,3 @@ for (let i = 0; i < choices.length; i++) {
 const resultDisplay = document.createElement('div');
 resultDisplay.id = 'result-display';
 document.body.appendChild(resultDisplay);
-    
-//     function game() {
-//         let wins = 0;
-//         let losses = 0;
-        
-//         while (wins + losses < 5) {
-//             let playerSelection = prompt("Choose Rock, Paper, or Scissors:");
-//             playerSelection = format(playerSelection);
-            
-//             while (!choices.includes(playerSelection)) {
-//                 playerSelection = prompt("Please choose ONLY Rock, Paper, or Scissors:");
-//                 playerSection = format(playerSelection);
-//             }
-
-//             let computerSelection = computerPlay();
-//             let result = playRound(playerSelection, computerSelection);
-//             console.log(result);
-            
-//             if (result.indexOf("Win") !== -1) {
-//                 wins++;
-//             }
-
-//             if (result.indexOf("Lose") !== -1) {
-//                 losses++;
-//             }
-//         }
-
-//     return `You ${wins > losses ? 'win' : 'lose'}, ${wins} to ${losses}.`;
-// }
-
-// let gameResults = game();
-// console.log(gameResults);
